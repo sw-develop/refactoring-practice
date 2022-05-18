367f01b51d55c4522ec0d0b38dadb94b7a1d505e
sw-develop <psw00719@naver.com>
2022-05-18 19:16:25 +0900

    refactor: 하위 클래스들의 중복된 코드를 부모 클래스의 메서드로 올리기

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,6 +1,11 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 
 public class Dashboard {
 
@@ -9,6 +14,21 @@ public class Dashboard {
         reviewerDashboard.printReviewers();
 
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
-        participantDashboard.printParticipants(15);
+        participantDashboard.printUsernames(15);
+    }
+
+    /*하위 클래스들의 중복된 코드를 부모 클래스의 메서드로 올림*/
+    public void printUsernames(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get users
+        Set<String> users = new HashSet<>();
+        issue.getComments().forEach(c -> users.add(c.getUserName()));
+
+        // Print users
+        users.forEach(System.out::println);
     }
 }
